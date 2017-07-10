b4f1fccc19245c8ed1d7e43339be25b3288f60d5
chemadent <b00902002@ntu.edu.tw>
2017-07-10 17:29:13 +0800

    first commit

---
+++ ledsFade.ino
@@ -0,0 +1,32 @@
+const int ledCount = 6;
+
+int ledPins[] = {
+  3, 5, 6,
+  9, 10, 11
+};  // an array of pin numbers to which LEDs are attached  
+  
+void setup(){
+  // loop over the pin array and set them all to output:
+  for (int thisLed = 0; thisLed < ledCount; thisLed++) {
+    pinMode(ledPins[thisLed], OUTPUT);
+  }
+}
+
+void loop(){
+    for (int thisLed = 0; thisLed < ledCount; thisLed++){
+        for (int i = 0; i < 255; i++){
+            analogWrite(ledPins[thisLed], i);
+            delay(3);
+        }
+        for (int i = 255; i > 0; i--){
+            analogWrite(ledPins[thisLed], i);
+            delay(3);
+        }
+  
+        delay(50);
+    //    digitalWrite(ledPins[thisLed], HIGH);
+    //    delay(800);
+    //    digitalWrite(ledPins[thisLed], LOW);
+    //    delay(50);
+    }
+}
